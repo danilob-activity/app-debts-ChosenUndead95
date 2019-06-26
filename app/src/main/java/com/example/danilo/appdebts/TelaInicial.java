e7e813f6f06a8180647fbd950f97c3550b3f8e29
Rodrigo Silveira <nohopeleft13@live.com>
2019-06-26 10:26:09 -0300

    0.000001

--- app/src/main/java/com/example/danilo/appdebts/TelaInicial.java
+++ app/src/main/java/com/example/danilo/appdebts/TelaInicial.java
@@ -1,13 +1,27 @@
 package com.example.danilo.appdebts;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.danilo.appdebts.database.DatabaseHelper;
+
 public class TelaInicial extends AppCompatActivity {
+    private SQLiteDatabase mConection;
+    private DatabaseHelper mDataHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+    }
+    private void createConnection() {
+        try {
+            mDataHelper = new DatabaseHelper(this);
+            mConection = mDataHelper.getWritableDatabase();
+            Snackbar.make(mLayout, R.string.sucess_conection, Snackbar.LENGTH_LONG).show();
+        } catch (SQLException e) {
+            Snackbar.make(mLayout, e.toString(), Snackbar. LENGTH_LONG).show();
+        }
     }
 }
