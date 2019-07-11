c33075ed8fb5b3aff94fc1eb5edf633f846f0415
Rodrigo Silveira <nohopeleft13@live.com>
2019-07-11 11:54:42 -0300

    0.8.5

--- app/src/main/java/com/example/danilo/appdebts/InsertDebts.java
+++ app/src/main/java/com/example/danilo/appdebts/InsertDebts.java
@@ -1,14 +1,23 @@
 package com.example.danilo.appdebts;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class InsertDebts extends AppCompatActivity {
-
+    EditText mEdtextDataPay;
+    final Calendar myCalendar = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +25,39 @@ public class InsertDebts extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled( true); //Mostrar o botão
         getSupportActionBar().setHomeButtonEnabled( true);//Ativar o botão
         getSupportActionBar().setTitle( R.string.titleInsert);
+
+        mEdtextDataPay= (EditText) findViewById(R.id.editTextDate);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            //    updateLabel();
+            }
+
+        };
+
+        mEdtextDataPay.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(InsertDebts.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+    private void updateLabel() {
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mEdtextDataPay.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
